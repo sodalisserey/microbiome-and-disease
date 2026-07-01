117fe3b5e1f3820dd3514768b565a4321723e360
Dalis <sodalisserey@gmail.com>
2026-07-01 15:52:32 +0100

    Combine LEfSe analysis/plotting scripts

--- scripts/02_lefse.R
+++ scripts/02_lefse.R
@@ -4,15 +4,15 @@
 # 3. Execute main functions:
 #   a. run_analysis()
 #     * Clean, extract disease and validate conditions
-#     * ANALYSIS BRANCH 1 (healthy x 1 disease) -> run_pipeline() executes:
-#           - run_qc() 
-#           - run_lefser() 
+#     * ANALYSIS BRANCH 1 (healthy x 1 disease) -> analysis_pipeline() executes:
+#           - qc_cohort() 
+#           - lefse_analyse() 
 #           - log_qc()
 #           - log_analysis()
 #           - log_result()
 #     * ANALYSIS BRANCH 2 (healthy x >1 disease)  -> 
 #           - Create pair-wise subsets
-#           - Execute run_pipeline()
+#           - Execute analysis_pipeline()
 #   b. export_analysis()
 #     * Save lefse_results, contingency_tables, qc_summary and analysis_log  
 #       as out_dir/[log].csv
@@ -21,22 +21,26 @@
 # TODO collapse branches via comparisons
 # TODO filter by sample size before analysis
 # TODO compile contingency table and QC into one output
+# TODO update documentation after combining with lefse_plot.R
 
 # Load packages and dependencies -----------------------------------------------
-library(rlang)
 library(curatedMetagenomicData)
 library(lefser)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(rlang)
 source("R/utils.R")
 
 
 # Define output directory ------------------------------------------------------
-out_dir <- "results/01_lefse_analyse"
+out_dir <- "results/02_lefse"
 
 
-# Define helper functions ------------------------------------------------------
+# Define analysis functions ----------------------------------------------------
 #' Run QC on a cohort and return a contingency table, class imbalance and 
 #' age imbalance ratios and counts
-run_qc <- function(
+qc_cohort <- function(
     cohort,
     cohort_name,
     disease,
@@ -156,7 +160,7 @@ run_qc <- function(
 #'    * at least 2 age groups are present
 #'    * all age groups contain both healthy and disease samples
 #' - LEfSe differential abundance analysis
-run_lefser <- function(
+lefse_analyse <- function(
     cohort,
     comparison = NULL,
     subclassCol = "age_category",
@@ -374,7 +378,7 @@ log_result <- function(
 }
 
 #' Run analysis pipeline which includes QC checks, LEfSe analysis and logging
-run_pipeline <- function(
+analysis_pipeline <- function(
     cohort,
     cohort_name,
     comparison_name,
@@ -382,7 +386,7 @@ run_pipeline <- function(
     analysis_res) {
   
   # Run and log QC
-  qc <- run_qc(
+  qc <- qc_cohort(
     cohort,
     cohort_name = cohort_name, 
     disease = disease)
@@ -395,7 +399,7 @@ run_pipeline <- function(
     analysis_res = analysis_res)
   
   # Run and log LEfSe
-  result <- run_lefser(
+  result <- lefse_analyse(
     cohort,
     comparison = paste("control vs", disease),
     disease_label = disease)
@@ -419,9 +423,236 @@ run_pipeline <- function(
 }
 
 
+# Define plotting functions ----------------------------------------------------
+#' Extract species label from full clade string
+extract_species <- function(df) {
+  df %>%
+    dplyr::mutate(
+      species = dplyr::case_when(
+        stringr::str_detect(features, "s__") ~ sub(".*s__", "s__", features),
+        stringr::str_detect(features, "g__") ~ sub(".*g__", "g__", features),
+        stringr::str_detect(features, "f__") ~ sub(".*f__", "f__", features),
+        TRUE ~ features),
+      
+      species = sub("\\|.*", "", species),
+      species = sub("^[a-z]__", "", species),
+      species = gsub("_", " ", species),
+      species = sub("^species:", "", species))
+}
+
+#' Convert disease label string with underscores to title case
+convert_smart_case <- function(x) {
+  x <- gsub("_", " ", x)
+  
+  words <- strsplit(x, " ")[[1]]
+  
+  words <- ifelse(
+    words == toupper(words),
+    words,
+    stringr::str_to_title(tolower(words)))
+  
+  paste(words, collapse = " ")
+}
+
+#' Prepare LEfSe plot data by filtering scores and assigning healthy/disease label
+process_scores <- function(
+    lefse_res_df,
+    disease_label,
+    healthy_label,
+    max_features) {
+  
+  lefse_res_df %>%
+    dplyr::mutate(scores = as.numeric(scores)) %>%
+    dplyr::filter(!is.na(scores)) %>%
+    dplyr::slice_max(
+      order_by = abs(scores),
+      n = max_features,
+      with_ties = FALSE) %>%
+    
+    dplyr::mutate(
+      group_label = ifelse(
+        scores > 0,
+        disease_label,
+        healthy_label),
+      
+      group_label = factor(
+        group_label,
+        levels = c(healthy_label, disease_label)))
+}
+
+#' Log plotting status and number of features
+log_plot <- function(
+    lefse_res_df,
+    comparison,
+    # TODO remove repeated code
+    min_features = 2,
+    max_features = 6) {
+  
+  create_log <- function(
+    comp,
+    n_features,
+    plotted_features = NA,
+    status,
+    reason = NA,
+    df_plot = NULL) {
+    
+    list(
+      log = data.frame(
+        comparison = comp,
+        n_features = n_features,
+        plotted_features = plotted_features,
+        status = status,
+        reason = reason,
+        stringsAsFactors = FALSE
+      ),
+      df_plot = df_plot
+    )
+  }
+  
+  n_features_raw <- nrow(lefse_res_df)
+  
+  # Case 1: no data
+  if (n_features_raw == 0) {
+    return(create_log(comparison, 0, NA, "SKIPPED", "No data"))
+  }
+  
+  disease_label <- convert_smart_case(unique(lefse_res_df$disease)[1])
+  healthy_label <- "Healthy"
+  
+  # Case 2: too few features
+  if (n_features_raw < min_features) {
+    return(create_log(
+      comparison,
+      n_features_raw,
+      NA,
+      "SKIPPED",
+      "Too few features"
+    ))
+  }
+  
+  # Process and filter scores for plotting
+  df_plot <- process_scores(
+    lefse_res_df,
+    disease_label,
+    healthy_label,
+    max_features
+  )
+  
+  n_features <- nrow(df_plot)
+  
+  # Case 3: no valid scores after processing
+  if (n_features == 0) {
+    return(create_log(
+      comparison,
+      n_features_raw,
+      0,
+      "SKIPPED",
+      "No valid scores"
+    ))
+  }
+  
+  # Case 4: set status as "READY" for plotting
+  create_log(
+    comparison,
+    n_features_raw,
+    n_features,
+    "READY",
+    NA,
+    df_plot = df_plot
+  )
+}
+
+#' Build a LEfSe bar plot using ggplot2
+create_plot <- function(
+    lefse_res_df,
+    cohort_name,
+    disease_label,
+    healthy_label) {
+  
+  ggplot2::ggplot(
+    lefse_res_df,
+    ggplot2::aes(
+      x = scores,
+      y = stats::reorder(species, scores, FUN = mean),
+      fill = group_label)) +
+    
+    ggplot2::geom_col(
+      width = 0.7,
+      colour = "white",
+      linewidth = 0.2) +
+    
+    ggplot2::geom_vline(
+      xintercept = 0,
+      linewidth = 0.4,
+      colour = "grey40") +
+    
+    ggplot2::scale_fill_manual(
+      values = c(
+        setNames("#c5cb93ff", healthy_label),
+        setNames("#dfa57eff", disease_label)),
+      
+      name = "Enriched in") +
+    
+    ggplot2::scale_x_continuous(
+      expand = ggplot2::expansion(mult = c(0.05, 0.05))) +
+    
+    ggplot2::theme_minimal(base_size = 14) +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(
+        face = "bold",
+        size = 14,
+        hjust = 0.5),
+      
+      axis.text.y = ggplot2::element_text(
+        size = 14,
+        face = "italic"),
+      
+      # TODO legend text size increase
+      legend.position = "bottom",
+      panel.grid.major.y = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank()) +
+    
+    ggplot2::labs(
+      title = paste0(
+        cohort_name,
+        " (",
+        healthy_label,
+        " vs ",
+        disease_label,
+        ")"
+      ),
+      x = "LDA score (log10)",
+      y = NULL)
+}
+
+#' Save a single plot as a pdf
+save_plot <- function(
+    plot,
+    comparison,
+    out_dir,
+    n_features) {
+  
+  safe_comp <- gsub("[/\\\\]", "", comparison)
+  
+  out_path <- file.path(
+    out_dir,
+    paste0(safe_comp, ".pdf"))
+  
+  plot_height <- max(4, n_features * 0.35 + 1.5)
+  
+  ggplot2::ggsave(
+    filename = out_path,
+    plot = plot,
+    width = 9,
+    height = plot_height,
+    device = "pdf")
+  
+  invisible(out_path)
+}
+
 # Define main functions --------------------------------------------------------
 #' Run main LEfSe loop for two analysis branches
-run_analysis <- function(primary_cohorts) {
+run_analysis <- function(primary_cohorts, out_dir) {
   
   # Initialise storage object
   analysis_res <- list()
@@ -453,7 +684,7 @@ run_analysis <- function(primary_cohorts) {
       comparison_name <- paste(cohort_name, disease, sep = "_")
       message("\n", cohort_name, ": healthy vs ", info$diseases)
       
-      analysis_res <- run_pipeline(
+      analysis_res <- analysis_pipeline(
         cohort = cohort,
         cohort_name = cohort_name,
         disease = disease,
@@ -476,7 +707,7 @@ run_analysis <- function(primary_cohorts) {
 
         comparison_name <- paste(cohort_name, disease, sep = "_")
         
-        analysis_res <- run_pipeline(cohort = cohort_subset,
+        analysis_res <- analysis_pipeline(cohort = cohort_subset,
                                  cohort_name = cohort_name,
                                  disease = disease,
                                  comparison_name = comparison_name,
@@ -491,13 +722,7 @@ run_analysis <- function(primary_cohorts) {
   })
   lapply(analysis_res$contingency_tables, dim)
   
-  return(analysis_res)
-}
-
-
-#' Export LEfSe analysis outputs as raw R objects and CSVs
-export_analysis <- function(analysis_res, out_dir) {
-  
+  # Export results
   dir.create("results", recursive = TRUE, showWarnings = FALSE)
   dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
   
@@ -508,17 +733,87 @@ export_analysis <- function(analysis_res, out_dir) {
   analysis_res$analysis_log <- dplyr::bind_rows(analysis_res$analysis_log)
   analysis_res$qc_summary <- dplyr::bind_rows(analysis_res$qc_summary)
   analysis_res$contingency_df <- dplyr::bind_rows(analysis_res$contingency_tables)
-  analysis_res$lefser_df <- dplyr::bind_rows(analysis_res$lefse_results)
+  analysis_res$lefse_df <- dplyr::bind_rows(analysis_res$lefse_results)
   
   # Write CSVs
   write_csvs(
     out_dir,
     list(
-      lefse_results = analysis_res$lefser_df,
+      lefse_results = analysis_res$lefse_df,
       contingency_tables = analysis_res$contingency_df,
       qc_summary = analysis_res$qc_summary,
       analysis_log = analysis_res$analysis_log))
+  
+  return(analysis_res)
 }
+
+run_plotting <- function(
+    analysis_res,
+    out_dir,
+    min_features = 2,
+    max_features = 10) {
+  
+  plot_dir <- file.path(out_dir, "plots")
+  dir.create(file.path(plot_dir), recursive = TRUE,
+             showWarnings = FALSE)
+  
+  lefse_res <- analysis_res$lefse_df
+  
+  lefse_res <- extract_species(lefse_res)
+  
+  plot_log <- lapply(
+    unique(lefse_res$comparison),
+    function(comp) {
+      
+      lefse_res_df <- filter(
+        lefse_res,
+        comparison == comp
+      )
+      
+      res <- log_plot(
+        lefse_res_df = lefse_res_df,
+        comparison = comp,
+        min_features = min_features,
+        max_features = max_features
+      )
+      
+      if (res$log$status == "READY") {
+        
+        cohort_name <- unique(lefse_res_df$cohort)[1]
+        disease_label <- convert_smart_case(unique(lefse_res_df$disease)[1])
+        
+        p <- create_plot(
+          res$df_plot,
+          cohort_name,
+          disease_label,
+          "Healthy"
+        )
+        
+        save_plot(
+          p,
+          comp,
+          plot_dir,
+          nrow(res$df_plot)
+        )
+        
+        res$log$status <- "PLOTTED"
+      }
+      
+      res$log
+    }
+  )
+  
+  plot_log_df <- bind_rows(plot_log)
+  
+  write.csv(
+    plot_log_df,
+    file.path(plot_dir, "_plot_log.csv"),
+    row.names = FALSE)
+  message("Done. Plots written to: ", plot_dir)
+  
+  invisible(plot_log_df)
+}
+
 
 
 # Load data --------------------------------------------------------------------
@@ -526,5 +821,6 @@ primary_cohorts <- readRDS("data/primary.rds")
 
 
 # Execute ----------------------------------------------------------------------
-analysis_res <- run_analysis(primary_cohorts)
-export_analysis(analysis_res, out_dir)
+analysis_res <- run_analysis(primary_cohorts, out_dir)
+run_plotting(analysis_res, out_dir)
+
